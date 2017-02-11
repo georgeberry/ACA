5729af709a83142c61bbd9f5185de802db5de7af
George Berry <george.e.berry@gmail.com>
2017-02-11 18:40:13 -0500

    some more national analyses

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
 # ACA
 Some code on ACA
 
-### Population data
+Read [this paper](http://www.nejm.org/doi/full/10.1056/NEJMsa1202099), particularly the model in Table 2.
+
+## Plan
+
+Get the data to estimate mortality changes.
+
+Table 2 gives mortality rates for different subgroups. We'll consider the poverty measure and race measure separately.
+
+## Assumptions
+
+1. Expanding Medicaid and providing insurance on exchanges has same effect.
+1. All demographic factors not explicitly controlled for are assumed to be the same
+
+## Data
+
+### Population
 
 Go [here](http://www.census.gov/data/datasets/2015/demo/popest/counties-detail.html) and download the United States county population data. ~112MB
 
-### Uninsurance data
+### Uninsured rate data
 
 Go [here](https://www.enrollamerica.org/research-maps/maps/changes-in-uninsured-rates-by-county/) and click on the download link at the bottom of the page.
+
+## Tables
+
+### Top 10 states
+
+|State Name    | Lives saved / 100k (lower) | Lives saved / 100k (mid) | Lives saved / 100k (upper) | Rep margin change |
+|:-------------|-----------------------:|---------------------:|-----------------------:|-----------------:|
+|Kentucky      |                13.20411|              29.65845|                76.67384|         0.0714564|
+|West Virginia |                12.96018|              29.11055|                75.25739|         0.1492815|
+|New Mexico    |                12.12728|              27.23974|                70.42092|         0.0193631|
+|Arkansas      |                11.75611|              26.40602|                68.26557|         0.0323303|
+|California    |                11.63223|              26.12777|                67.54623|        -0.0687168|
+|Michigan      |                11.48318|              25.79298|                66.68073|         0.0971912|
+|Oregon        |                11.45434|              25.72821|                66.51328|         0.0111164|
+|Montana       |                10.33668|              23.21778|                60.02323|         0.0657907|
+|Ohio          |                10.26007|              23.04570|                59.57837|         0.1105010|
+|Nevada        |                10.17475|              22.85406|                59.08294|         0.0426381|
+
+
 
 # License
 
--- nationwide_changes.R
+++ nationwide_changes.R
@@ -1,8 +1,19 @@
-library(dplyr)
 library(stringr)
+library(ggplot2)
+library(dtplyr)
+library(knitr)
 
-pop_df = read.csv("cc-est2015-alldata.csv")
-uninsured_df = read.csv("County_Data_2016.csv") %>%
+twenty_twelve_county_results = read.csv("/Users/g/Desktop/2012_0_0_2.csv") %>%
+  select(fips, vote1, vote2, totalvote)
+twenty_sixteen_county_results = read.csv("/Users/g/Desktop/2016_0_0_2.csv") %>%
+  select(fips, vote1, vote2, totalvote)
+twenty_twelve_state_results = read.csv("/Users/g/Desktop/2012_0_0_1.csv") %>%
+  select(name, vote1, vote2, totalvote)
+twenty_sixteen_state_results = read.csv("/Users/g/Desktop/2016_0_0_1.csv") %>%
+  select(name, vote1, vote2, totalvote)
+
+pop_df = read.csv("/Users/g/Desktop/cc-est2015-alldata.csv")
+uninsured_df = read.csv("/Users/g/Desktop/County_Data_2016.csv") %>%
   mutate(county_fips = as.character(county_fips))
 
 # This is a back-of-the-envelope estimate, so we make simplifying assumptions
@@ -20,7 +31,22 @@ uninsured_df = read.csv("County_Data_2016.csv") %>%
 # Reserch article: http://www.nejm.org/doi/full/10.1056/NEJMsa1202099
 # WaPo article: https://www.washingtonpost.com/posteverything/wp/2017/01/23/repealing-the-affordable-care-act-will-kill-more-than-43000-people-annually/
 
-# Select NY state, 2013 estimate, all ages, only counties in 23rd
+# County election results
+# vote1 is Democrat, vote2 is Republican
+county_election_results = twenty_twelve_county_results %>%
+  left_join(twenty_sixteen_county_results, by='fips') %>%
+  mutate(fips=as.character(fips))
+colnames(county_election_results) = c('fips',
+                                      'dem2012',
+                                      'rep2012',
+                                      'total2012',
+                                      'dem2016',
+                                      'rep2016',
+                                      'total2016')
+county_election_results = county_election_results %>%
+  mutate(rep_margin_change = (rep2016 / total2016 - dem2016 / total2016) - (rep2012 / total2012 - dem2012 / total2012))
+
+# Select NY state, 2013 estimate, all ages
 pop_df = pop_df %>%
   select(STATE, COUNTY, STNAME, CTYNAME, YEAR, TOT_POP, AGEGRP) %>%
   filter(YEAR==6, AGEGRP==0) %>%
@@ -28,25 +54,84 @@ pop_df = pop_df %>%
          COUNTY = str_pad(COUNTY, 3, pad="0"),
          FIPS = paste(STATE, COUNTY, sep=""))
 
-
 # Join on county name and compute basic statistics
 agg_df = pop_df %>%
   left_join(uninsured_df, by=c("FIPS" = "county_fips")) %>%
   mutate(num_covered = -0.01 * decrease.from.2013.to.2016 * TOT_POP,
-         lives_saved_lower = num_covered / 455 / TOT_POP * 100000,
-         lives_saved_upper = num_covered / 176 / TOT_POP * 100000)
+         lives_saved_lower = num_covered / 1022 / TOT_POP * 100000,
+         lives_saved_mid = num_covered / 455 / TOT_POP * 100000,
+         lives_saved_upper = num_covered / 176 / TOT_POP * 100000) %>%
+  .[complete.cases(.),] %>%
+  select(state_abbrev,
+         CTYNAME,
+         STNAME,
+         TOT_POP,
+         FIPS,
+         decrease.from.2013.to.2016,
+         lives_saved_lower,
+         lives_saved_mid,
+         lives_saved_upper) %>%
+  arrange(-lives_saved_mid) %>%
+  left_join(county_election_results, by=c("FIPS"="fips"))
+
+# Find top 10
+
+top_ten = head(agg_df, n=10)
+
+# County correlation
+
+ggplot(agg_df, aes(x=lives_saved_mid, y=rep_margin_change)) +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+summary(lm(rep_margin_change ~ lives_saved_mid, data=agg_df))
+
+# State level
+
+# State election results
+state_election_results = twenty_twelve_state_results %>%
+  left_join(twenty_sixteen_state_results, by='name')
+colnames(state_election_results) = c('name',
+                                     'dem2012',
+                                     'rep2012',
+                                     'total2012',
+                                     'dem2016',
+                                     'rep2016',
+                                     'total2016')
+state_election_results = state_election_results %>%
+  mutate(rep_share = rep2016 / total2016,
+         rep_margin_change = (rep2016 / total2016 - dem2016 / total2016) - (rep2012 / total2012 - dem2012 / total2012))
+
+state_df = agg_df %>%
+  group_by(STNAME) %>%
+  mutate(state_pop = sum(TOT_POP),
+         state_frac = TOT_POP / state_pop,
+         state_frac_lives_saved_lower = state_frac * lives_saved_lower,
+         state_frac_lives_saved_mid = state_frac * lives_saved_mid,
+         state_frac_lives_saved_upper = state_frac * lives_saved_upper
+         ) %>%
+  summarize(state_lives_saved_lower = sum(state_frac_lives_saved_lower),
+            state_lives_saved_mid = sum(state_frac_lives_saved_mid),
+            state_lives_saved_upper = sum(state_frac_lives_saved_upper)) %>%
+  arrange(-state_lives_saved_mid) %>%
+  left_join(state_election_results, by=c("STNAME" = "name"))
+
+ggplot(state_df, aes(x=state_lives_saved_mid, y=rep_margin_change)) +
+  geom_point()
+
+summary(lm(rep_share ~ state_lives_saved_mid, data=state_df))
+
+# Summarize
+
+kable(head(state_df[,c('STNAME',
+                       'state_lives_saved_lower',
+                       'state_lives_saved_mid',
+                       'state_lives_saved_upper',
+                       'rep_margin_change')], n=10))
 
 # Map stuff here
 
-data(county.fips)
-color_mapping = colorRamp(c("white", "blue"))
+county_map = data.table(map_data('county'))
+setkey(county_map, region, subregion)
 
-counties = county.fips %>%
-  mutate(fips = as.character(fips)) %>%
-  left_join(agg_df, by=c("fips"="FIPS")) %>%
-  select(fips, polyname, lives_saved_lower) %>%
-  .[complete.cases(.),] %>%
-  filter(lives_saved_lower >= 0) %>%
-  mutate(color = rgb(color_mapping(lives_saved_lower / max(lives_saved_lower))/255))
-
-map('county', fill=T, col=counties$color)
+ggplot(county_map, aes(x=long, y=lat, group=group)) + geom_polygon() + coord_map()
